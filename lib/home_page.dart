ae83a8a4decc8c6d024516a4b4b82435ce116cb6
Eóin McMahon <emcmahon478@gmail.com>
2019-01-30 22:46:50 +0000

    Fixed employee add bug.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import './employee.dart';
 import 'dart:async';
+import 'text_field_custom.dart';
 
 
 class HomePage extends StatefulWidget
@@ -12,51 +13,56 @@ class HomePage extends StatefulWidget
 
 class _HomePageState extends State<HomePage>
 {
+
   TimeOfDay _time = new TimeOfDay.now();
   TimeOfDay _startTime = new TimeOfDay.now();
   TimeOfDay _endTime = new TimeOfDay.now();
+  List<TimeOfDay> times = [];
 
-
+  double totalTime = 0;
   Future<Null> _selectStartTime(BuildContext context, _time) async
   {
-    final TimeOfDay picked = await showTimePicker(
+
+    final TimeOfDay startPicked = await showTimePicker(
       context: context,
       initialTime: _time
     );
 
-    if (picked != null && picked != _time)
+    if (startPicked != null && startPicked != _time)
     {
       setState((){
-        _startTime = picked;
-        employees.add(_startTime.toString());
-
+        _startTime = startPicked;
+        times.add(startPicked);
+        print("Added start time: ${_startTime} to times");
+        print("endPicked: " + startPicked.toString());
         print('Start Time: ${_startTime.toString()}');
       });
     }
     
   }
+
   Future<Null> _selectEndTime(BuildContext context, _time) async
   {
-    final TimeOfDay picked = await showTimePicker(
+    final TimeOfDay endPicked = await showTimePicker(
       context: context,
       initialTime: _time
     );
 
-    if (picked != null && picked !=_time)
+    if (endPicked != null && endPicked !=_time)
     {
       setState((){
-        _endTime = picked;
-        employees.add(_endTime.toString());
-
+        _endTime = endPicked;
+        times.add(endPicked);
+        print("Added end time: ${_endTime} to times");
+        print("endPicked: " + endPicked.toString());
         print('End Time: ${_endTime.toString()}');
-        print(employees.toString());
+        
       });
     }
     
   }
 
 
-  List<String> employees = [];
   @override
   Widget build(BuildContext context) {      
     return Scaffold(
@@ -67,7 +73,7 @@ class _HomePageState extends State<HomePage>
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: Icon(Icons.person_add),
       onPressed: (){
         showDialog(
           barrierDismissible: false,
@@ -92,8 +98,6 @@ class _HomePageState extends State<HomePage>
                   child: Text("End time"),
                   onPressed: (){_selectEndTime(context, _time);},
                   color: Colors.blue,
-
-
                 )
                 // TimePicker(),
               ],
@@ -109,8 +113,15 @@ class _HomePageState extends State<HomePage>
             FlatButton(
               onPressed: () {
                 setState(() {
-                  employees.add(text);
+                  Map<String, List<TimeOfDay>> employees = {};
+                  print("Got this far");
+                  print("Times Selected: ${times.toString()}");
+                  print(times);
+                  print(text);
                   print(employees);
+                  employees[text] = times;
+                  print(employees);
+                  times.clear();
                 });
                 Navigator.of(context).pop();
               },
@@ -124,42 +135,15 @@ class _HomePageState extends State<HomePage>
     ,),      
     );
   }
+  Duration getDifference(TimeOfDay _start, TimeOfDay _end)
+  {
+    DateTime start;
+    DateTime end;
+    start = new DateTime(_start.hour, start.minute);
+    end = new DateTime(_end.hour, end.minute);
+
+    return end.difference(start);
+  }
 
 }  
-String text = '';
 
-
-
-
-class MyPrefilledText extends StatefulWidget {
-  @override
-  _MyPrefilledTextState createState() => _MyPrefilledTextState();
-}
-
-class _MyPrefilledTextState extends State<MyPrefilledText> {
-
-  TextEditingController _textController;
-
-
-
-  @override
-  void initState() {
-    super.initState();
-    _textController = TextEditingController();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoTextField(
-      // style: TextStyle(color: Colors.blue),
-      controller: _textController,
-      placeholder: "employee name", 
-      onChanged: (String e) {
-        setState(() {
-          text = e;
-          print(e);
-        });
-      },
-    );
-  }
-}
---
+++ lib/text_field_custom.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import './employee.dart';
+import 'dart:async';
+
+String text = '';
+
+class MyPrefilledText extends StatefulWidget {
+  @override
+  _MyPrefilledTextState createState() => _MyPrefilledTextState();
+}
+
+class _MyPrefilledTextState extends State<MyPrefilledText> {
+
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTextField(
+      // style: TextStyle(color: Colors.blue),
+      controller: _textController,
+      placeholder: "employee name", 
+      onChanged: (String e) {
+        setState(() {
+          text = e;
+          print(e);
+        });
+      },
+    );
+  }
+}
